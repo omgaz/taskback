fea82960bfd56242985bbbe3503766d99091d183
Gary Chisholm <garyjchisholm@gmail.com>
2013-02-15 21:54:56 +0000

    tutorial 2

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function() {
+  var config = {};
+  config.apiKey = 'your API key';
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientId = 'your client ID';
+  return config;
+});
