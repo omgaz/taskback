c59f9c5de62238e70ad9a2ba5f3a731b8ab091a6
Gary Chisholm <garyjchisholm@gmail.com>
2013-04-07 23:46:43 +0200

    Removed my key

--- app/js/config.js
+++ app/js/config.js
@@ -1,8 +1,8 @@
 define([], function() {
   var config = {};
-  config.apiKey = 'AIzaSyB4DePaYtU3mVqzK3IoiCeKxF94pV0qaq8';
+  config.apiKey = 'yourGoogleAPIKey';
   config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
-  config.clientId = '794098456712.apps.googleusercontent.com';
+  config.clientId = 'yourGoogleClientID.apps.googleusercontent.com';
 
   _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g
